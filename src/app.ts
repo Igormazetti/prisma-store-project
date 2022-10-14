80a5271ab529f709ac237e6390ebbb5db63421e6
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2022-10-14 15:17:02 -0300

    Alterada estrutura de pastas

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import router from "./modules/products/routes/ProductsRoutes";
-
-const app = express();
-
-app.use(express.json());
-app.use("/products", router);
-
-export default app;
