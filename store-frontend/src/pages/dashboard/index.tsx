aaec5babd67e524f412a94d0c86fb920b69c7fe3
Igor <iigormazetti@hotmail.com>
2022-10-20 17:46:42 -0300

    adicionado arquivo .envexample no back

---
+++ store-frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+
+export default function Dashboard() {
+  return (
+    <Box bg='brand.900' w='100%' p={4} color='white'>
+    This is the Box
+    </Box>
+  )
+}
---
+++ store-frontend/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { routes } from '../routes'
+
+const Home: NextPage = () => {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    push({ pathname: routes.dashboard })
+  }, [push])
+  return <></>
+}
+
+
+export default Home
