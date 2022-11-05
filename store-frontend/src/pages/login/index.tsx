4048ccf95ead00abcbac4072a4c62d207c3e4bfd
Igor <iigormazetti@hotmail.com>
2022-11-05 10:32:22 -0300

    feat: finalizado formulário de login, criada rota e tela para criar conta

--- store-frontend/src/pages/login/index.tsx
+++ store-frontend/src/pages/login/index.tsx
@@ -1,10 +1,17 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Router from "next/router";
 
 export default function Login() {
+  const redirectCriarConta = () => {
+    Router.push("/criarconta");
+  };
+
   return (
     <Flex
-      bg="rgb(0,0,1);"
-      bgImage="linear-gradient(128deg, rgba(0,0,1,1) 0%, rgba(0,1,3,1) 67%, rgba(3,40,43,1) 92%, rgba(6,61,64,1) 100%)"
+      bg="rgb(215,215,215);"
+      bgImage="linear-gradient(144deg, rgba(167,165,165,1) 64%, rgba(18,68,82,1) 97%)"
       w="100%"
       h="full"
       p={4}
@@ -12,16 +19,78 @@ export default function Login() {
       align="center"
       justify="center"
     >
-      <Box
-        bg="linear-gradient(50deg, rgba(16,18,18,1) 0%, rgba(16,18,18,1) 54%, rgba(17,37,40,1) 70%, rgba(9,58,61,1) 98%)"
+      <Flex
+        bg="rgba(255, 255, 255, .3)"
+        backdrop-filter="blur(5px)"
+        justifyContent="center"
         maxW="600"
         w="full"
-        h="500"
-        display="flex"
+        h="600"
         rounded="25"
         opacity="0.5"
-        p="25"
-      ></Box>
+        p="15"
+      >
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+            password: Yup.string()
+              .min(5, "Senha deve ter ao menos cinco caracteres")
+              .max(10, "Senha deve ter no máximo dez caracteres")
+              .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="formularios">
+              <label className="form-label" htmlFor="email">
+                Email
+                <Field className="formik-input" type="email" name="email" />
+              </label>
+              <ErrorMessage name="email" component="div" />
+
+              <label className="form-label" htmlFor="password">
+                Senha
+                <Field
+                  className="formik-input"
+                  type="password"
+                  name="password"
+                />
+              </label>
+              <ErrorMessage name="password" component="div" />
+
+              <Box
+                as="button"
+                h="50"
+                w="100%"
+                type="submit"
+                disabled={isSubmitting}
+                bg="#58a5bd"
+              >
+                Entrar
+              </Box>
+              <Box
+                h="50"
+                color="#000102"
+                bg="#58a5bd"
+                w="100%"
+                as="button"
+                type="button"
+                onClick={redirectCriarConta}
+              >
+                Criar conta
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </Flex>
     </Flex>
   );
 }
