0793cdc4e54ad6b331a9864ad55dec4a5f9a8403
Igor <iigormazetti@hotmail.com>
2022-11-22 06:39:55 -0300

    fix: alterada requisição

--- store-frontend/src/service/login.ts
+++ store-frontend/src/service/login.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function login(email: string, password: string): Promise<User> {
   const url = 'http://localhost:6060/user/login';
-  const request = await axios
+  const request = axios
     .post(url, {
       email,
       password,
