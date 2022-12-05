663f5af2791bed14eda3d89b7ab836689e5c6e69
Igor <iigormazetti@hotmail.com>
2022-12-05 19:06:08 -0300

    feat: criado wrapper das páginas principais

--- store-frontend/src/pages/dashboard/index.tsx
+++ store-frontend/src/pages/dashboard/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Box, Flex, Text } from '@chakra-ui/react';
+import ViewWrapperLayout from 'components/ViewWrapperLayout';
 
 export default function Dashboard() {
   return (
-    <Flex
-      bg="rgb(215,215,215);"
-      bgImage="linear-gradient(144deg, #d6d3d3 64%, rgba(17,64,77,1) 97%)"
-      w="100%"
-      h="full"
-      p={4}
-    >
+    <ViewWrapperLayout title="Home">
       <Box as="header" width="100%" height="100px" border="1px solid black">
         <Text>Dashboard Page</Text>;
       </Box>
-    </Flex>
+    </ViewWrapperLayout>
   );
 }
