6a4d411f1dd3ea201893f5e5cf56ea50d8e022b3
Igor <iigormazetti@hotmail.com>
2023-01-10 22:29:01 -0300

    refactor: refatorada função de criar produtos no back-end

--- back-end/src/modules/products/controllers/ProductController.ts
+++ back-end/src/modules/products/controllers/ProductController.ts
@@ -1,22 +1,15 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 import CreateProductService from '../services/CreateProduct.service';
 import GetAllProductsService from '../services/GetAllProducts.service';
 import GetProductByIdService from '../services/GetProductById.service';
 import UpdateProductByIdService from '../services/UpdateProductById.service';
-import { container } from 'tsyringe';
 
 export default class ProductController {
   public async create(request: Request, response: Response) {
-    const { title, quantity, companyId, value } = request.body;
-
     const createProductService = container.resolve(CreateProductService);
 
-    const product = await createProductService.execute(
-      title,
-      quantity,
-      companyId,
-      value,
-    );
+    const product = await createProductService.execute(request.body);
 
     return response.status(200).json(product);
   }
